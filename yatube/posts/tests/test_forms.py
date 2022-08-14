72cb796d2ba6aca8261aa62f81d2e47460e7f054
Aleksandr Miheichev <ya.saska20@gmail.com>
2022-08-15 00:15:00 +0300

    Sprint_6 project_iteration_2_1

--- yatube/posts/tests/test_forms.py
+++ yatube/posts/tests/test_forms.py
@@ -3,7 +3,6 @@ import tempfile
 
 from django import forms
 from django.conf import settings
-from django.core.cache import cache
 from django.core.files.uploadedfile import SimpleUploadedFile
 from django.test import Client, TestCase, override_settings
 from django.urls import reverse
